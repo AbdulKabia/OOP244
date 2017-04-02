a8a4c29b41fbb4ec294813ed322209fd581c9edc
AbdulKabia <akkabia@gmail.com>
2017-04-02 16:56:34 -0400

    Add files via upload

---
+++ Milestone3/Streamable.h
@@ -0,0 +1,17 @@
+#ifndef SICT__Streamable_H_
+#define SICT__Streamable_H_
+#include <iostream>
+#include <fstream>
+#include "MyFile.h"
+using namespace std;
+namespace sict{
+  class Streamable{
+  public:
+    // pure virtual functions go here!
+	  virtual fstream& store(fstream& file, bool addNewLine = true)const = 0;
+	  virtual fstream& load(std::fstream& file) = 0;
+	  virtual ostream& write(ostream& os, bool linear)const = 0;
+	  virtual istream& read(istream& is) = 0;
+  };
+}
+#endif
